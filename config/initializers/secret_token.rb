eb0119e1c7813dad517fe82eb58cc289110ed6db
Charles Wright <charles.wright@gmail.com>
2011-08-19 13:08:02 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseMongo::Application.config.secret_token = '956ed57da20c953d30f8126b509f37555022ba0130cdcb885241d9862bf3f4629e4403c8cfbfd53087cc4508d395c9425e23ca1d651f56acd9db422c399f0bd3'
